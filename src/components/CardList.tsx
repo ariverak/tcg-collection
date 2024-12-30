060fab569ebd1b3416b84cbb21ad9da107c4d8a9
Andrés Rivera <jrivera.v@outlook.com>
2024-12-30 02:02:44 -0300

    fix

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -7,7 +7,7 @@ const CardList = ({ cards }: { cards: any[] }) => {
   return (
     <Row gutter={[16, 16]} justify='center'>
       {cards.map((pokemon, index) => (
-        <Col xs={24} sm={12} md={8} lg={5} key={index}>
+        <Col xs={12} sm={12} md={8} lg={5} key={index}>
           <Card frontImage={pokemon} flippedDelay={index * 50} />
         </Col>
       ))}
